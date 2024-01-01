e3e3c9864f01876e53b16a7fdb2e76494f7aab4c
Deepandra Pateireya <dpateireya@gmail.com>
2024-01-01 09:33:59 +0530

    add route page component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./navbar";
-import Welcome from "./Welcome";
-import Accordion from "./accordion/Accordion";
-import Keepnote from "./keepnote/Keepnote";
-import Showads from "./showads/Showads";
-import Incredecre from "./incredecre/Incredecre";
-import Timer from "./timer/Timer";
-import Todo from "./todo/todo1/Todo";
+import RoutePage from "./navrouter/RoutePage";
+
 function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/accordion" element={<Accordion />} />
-        <Route path="/showads" element={<Showads />} />
-        <Route path="/incredecre" element={<Incredecre />} />
-        <Route path="/timer" element={<Timer />} />
-        <Route path="/todo" element={<Todo />} />
-        <Route path="/keepnote" element={<Keepnote />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RoutePage />;
 }
 
 export default App;
